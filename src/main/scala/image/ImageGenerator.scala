4801f3b3a73207d85eabae23bfc06b701939c0be
Jean Detoeuf <jean@upcoding.fr>
2019-08-13 15:01:10 +0200

    put image parts into methods

--- src/main/scala/image/ImageGenerator.scala
+++ src/main/scala/image/ImageGenerator.scala
@@ -11,53 +11,66 @@ import zio.ZIO
 import scala.util.Try
 
 object ImageGenerator {
-  val scalaIORed = new Color(0xbc1321)
+  private val scalaIORed = new Color(0xbc1321)
+  private val transparent = new Color(0, 0, 0, 0)
+  private val montserrat40Plain = new Font("Montserrat", Font.PLAIN, 40)
+  private val ratio = 3
+  private val h = 267 * ratio
+  private val w = 507 * ratio
 
   def of(imageDetails: ImageDetails) = {
-    val h = 267 * 3
-    val w = 507 * 3
-    val timesRoman = "Arial Black"
-
     val img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
-
     val g = img.getGraphics.asInstanceOf[Graphics2D]
-    g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-    g.setBackground(Color.black)
-
-    g.setColor(scalaIORed)
-    for (offset <- 1 until 3) {
-      g.drawLine(50, 250 + offset, 200, 250 + offset)
-    }
-
-    val profilePicture: BufferedImage = ImageIO.read(imageDetails.speakerPicture)
-    g.drawImage(profilePicture, w - h, 0, h, h, Color.black, null)
-
-    val transparent = new Color(0, 0, 0, 0)
-    val gradientWidth = 100
-    g.setPaint(new GradientPaint(w - h, 0, Color.black, w - h + gradientWidth, 0, transparent))
-    g.fillRect(0, 0, w - h + gradientWidth, h)
-
-    val scalaIOPicture: BufferedImage =
-      ImageIO.read(new File(this.getClass.getResource("scalaio_black.png").getFile))
-    g.drawImage(scalaIOPicture, 50, h - 300, 600, 300, Color.black, null)
-
-    g.setColor(scalaIORed)
-    g.setClip(0, 0, w / 2, h)
-    g.setFont(new Font(timesRoman, Font.PLAIN, 40))
-    drawMultilineString(g, imageDetails.talkTitle, 31, 50, 100)
-
-    g.setColor(scalaIORed)
-    g.setFont(new Font(timesRoman, Font.PLAIN, 24))
-    drawMultilineString(g, imageDetails.speakerName, 31, 50, h / 2)
+    initializeCanvas(g)
+    drawProfilePicture(g, imageDetails)
+    drawScalaIOLogo(g)
+    drawTalkTitle(g, imageDetails)
+    drawSpeakerName(g, imageDetails)
 
     ZIO.fromTry(
       Try(ImageIO.write(img, "png", new File(s"/Users/jeandetoeuf/Desktop/accepted/${imageDetails.speakerName}.png")))
     )
   }
 
+  private def initializeCanvas(g: Graphics2D) = {
+    g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    g.setBackground(Color.black)
+  }
+
+  private def drawProfilePicture(g: Graphics2D, imageDetails: ImageDetails): Unit = {
+    val profilePicture: BufferedImage = ImageIO.read(imageDetails.speakerPicture)
+    g.drawImage(profilePicture, w - h, 0, h, h, Color.black, null)
+    val speakerPictureGradientWidth = 100
+    g.setPaint(new GradientPaint(w - h, 0, Color.black, w - h + speakerPictureGradientWidth, 0, transparent))
+    g.fillRect(0, 0, w - h + speakerPictureGradientWidth, h)
+  }
+
+  private def drawScalaIOLogo(g: Graphics2D) = {
+    val scalaIOPicture: BufferedImage =
+      ImageIO.read(new File(this.getClass.getResource("scalaio_black.png").getFile))
+    g.drawImage(scalaIOPicture, 50, h - 300, 600, 300, Color.black, null)
+  }
+
+  private def drawTalkTitle(g: Graphics2D, imageDetails: ImageDetails): Unit = {
+    g.setColor(scalaIORed)
+    g.setFont(montserrat40Plain)
+    drawMultilineString(g, imageDetails.talkTitle, 31, 50, 100)
+  }
+
+  private def drawSpeakerName(g: Graphics2D, imageDetails: ImageDetails): Unit = {
+    val speakerNameGradientWidth = w - h - 100
+    g.setPaint(new GradientPaint(speakerNameGradientWidth / 2, 0, scalaIORed, speakerNameGradientWidth, 0, transparent))
+    val lineHeight = g.getFontMetrics().getHeight
+    g.fillRect(0, h / 2 - lineHeight, speakerNameGradientWidth, (lineHeight * 1.5).intValue())
+
+    g.setColor(Color.white)
+    g.setFont(montserrat40Plain)
+    drawMultilineString(g, imageDetails.speakerName, 31, 50, h / 2)
+  }
+
   private def drawMultilineString(g: Graphics2D, text: String, wrapLength: Int, x: Int, y: Int) = {
-    val lineHeight = g.getFontMetrics().getHeight()
+    val lineHeight = g.getFontMetrics().getHeight
     WordUtils
       .wrap(text, wrapLength)
       .split("\n")
